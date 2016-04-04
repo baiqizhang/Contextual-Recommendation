9f92e5f531df4ffff33ed3f87f277cefa7dbaeb3
baiqizhang <baiqiz@andrew.cmu.edu>
2016-04-03 21:48:35 -0700

    cleanup

--- pre_filter.py
+++ pre_filter.py
@@ -6,8 +6,6 @@ item_set = set()
 
 user_rate_list = {}  # map (uid) -> [(iid,rating,loc,time)]
 
-
-# similarity
 def cosine_sim(user_1, user_2, time='ANY', location='ANY'):
     """
     cosine similarity between users using pre-filtering
@@ -25,6 +23,7 @@ def cosine_sim(user_1, user_2, time='ANY', location='ANY'):
     for item in user_rate_list[user_2]:
         if (time == 'ANY' or time == item[2]) and (location == 'ANY' or location == item[3]):
             score_2[item[0]] = int(item[1])
+
     # in case of duplication:
     for id in score_1:
         N1 += score_1[id] * score_1[id]
@@ -54,7 +53,6 @@ def recommend(user, time='ANY', location='ANY'):
         if user == other_user:
             continue
         sum += cosine_sim(user, other_user,time,location)
-        # print user,other_user,cosine_sim(user, other_user,time,location)
 
     score_final = {} # = sum{score/count}
     for other_user in user_rate_list:
@@ -85,11 +83,11 @@ def recommend(user, time='ANY', location='ANY'):
             else:
                 score_final[id] = score[id]/count[id]
 
-    # soring
+    # sorting
     result = []
     for id in score_final:
         result.append((id, score_final[id]))
-        # print id,":",score[id]
+
     result.sort(key=lambda x: x[1], reverse=True)
     return result
 
@@ -106,13 +104,13 @@ if __name__ == '__main__':
             rating = row['rating']
             user_set.add(userid)
             item_set.add(itemid)
+            # only use data where time and location are both available
             if location != 'NA' and time != 'NA':
                 if user_rate_list.has_key(userid):
                     user_rate_list[userid].append((itemid, rating, time, location))
                 else:
                     user_rate_list[userid] = [(itemid, rating, time, location)]
 
-    # print cosine_sim('1032', '1116')
     print 'user count =',len(user_set)
     print 'item count =',len(item_set)
 
