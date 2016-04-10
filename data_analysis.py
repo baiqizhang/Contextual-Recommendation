468e7fc210c8032f2b0591572c707e4a799a3a05
Hywel Wong <whywell@users.noreply.github.com>
2016-04-10 00:29:49 -0700

    Updated with cosine similarity

--- data_analysis.py
+++ data_analysis.py
@@ -1,26 +1,34 @@
 import pandas as pd
+from scipy.spatial.distance import cosine
 import numpy as np
 
-def main():
+def recommend(targetUser):
     # See example here: http://tungwaiyip.info/2012/Collaborative%20Filtering.html
 
     df = pd.read_csv("frappe.csv", "\t")
-    print df
     df.drop(df.columns[range(3,11)], axis=1, inplace=True)
-    rating = df.groupby(["user", "item"])["cnt"].sum().reset_index()
-    ratingP =  rating.pivot_table("cnt", "item", "user")
+    ratingList = df.groupby(["user", "item"])["cnt"].sum().reset_index()
+    ratingMatrix = ratingList.pivot_table("cnt", "item", "user")
+    ratingMatrix = ratingMatrix.fillna(0)
 
-    rating0 = ratingP[0]
-    sim_0 =  ratingP.corrwith(rating0)
+    # Compute Cosine Similarity
+    ratingUser = ratingMatrix[targetUser]
+    cosine_sim = []
+    for cIndex in ratingMatrix.columns:
+        cosine_sim.append(1 - cosine(ratingUser, ratingMatrix[cIndex]))
+    cosine_sim = pd.Series(cosine_sim)
 
-    rating_c = rating[(rating0[rating.item].isnull().values) & (rating.user != 0)]
-    print rating_c
-    rating_c['similarity'] = rating_c['user'].map(sim_0.get)
-    print rating_c
-    rating_c['sim_rating'] = rating_c.similarity * rating_c.cnt
+    # Compute pairwise correlation
+    # sim_0 =  ratingP.corrwith(ratingUser)
 
-    recommendation = rating_c.groupby('item').apply(lambda s: s.sim_rating.sum() / s.similarity.sum())
-    print recommendation.sort_values(ascending=False)
+    # Include only items not consumed by user yet and remove rows for user
+    notConsumed = (ratingUser[ratingList.item] == 0).values # True/False array
+    ratingListNew = ratingList[notConsumed & (ratingList.user != targetUser)]
+    ratingListNew['similarity'] = ratingListNew['user'].map(cosine_sim.get)
+    ratingListNew['sim_rating'] = ratingListNew.similarity * ratingListNew.cnt
+
+    recommendation = ratingListNew.groupby('item').apply(lambda row: row.sim_rating.sum() / row.similarity.sum())
+    return recommendation.sort_values(ascending=False)[:10]
 
 
-main()
+print recommend(936)
