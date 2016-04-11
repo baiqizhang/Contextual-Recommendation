b7a6e6634393c4dd7208e93ef7e96d1137da6aa8
baiqizhang <baiqiz@andrew.cmu.edu>
2016-04-10 23:53:34 -0700

    evaluation using nDCG10, 25% data set as ground truth

--- pre_filter_frappe.py
+++ pre_filter_frappe.py
@@ -5,6 +5,7 @@ user_set = set()
 item_set = set()
 
 user_rate_list = {}  # map (uid) -> [(iid,rating,loc,time)]
+user_rate_list_ground_truth = {}  # map (uid) -> [(iid,rating,loc,time)]
 
 def cosine_sim(user_1, user_2, time='ANY', location='ANY'):
     """
@@ -83,16 +84,24 @@ def recommend(user, time='ANY', location='ANY'):
             else:
                 score_final[id] = score[id]/count[id]
 
+    # filter out items
+    viewed = {}
+    for item in user_rate_list[userid]:
+        viewed[item[0]] = 1
+
+
     # sorting
     result = []
     for id in score_final:
-        result.append((id, score_final[id]))
+        if not viewed.has_key(id):
+            result.append((id, score_final[id]))
 
     result.sort(key=lambda x: x[1], reverse=True)
     return result
 
 # main
 if __name__ == '__main__':
+
     with open('frappe.txt', 'r') as csvfile:
         reader = csv.DictReader(csvfile, delimiter='\t')
 
@@ -104,18 +113,72 @@ if __name__ == '__main__':
             rating = row['cnt']
             user_set.add(userid)
             item_set.add(itemid)
+
+            ignore_flag = 0
+            if np.random.random()<0.5:
+                ignore_flag = 1
+
             # only use data where time and location are both available
             if location != 'NA' and time != 'NA':
-                if user_rate_list.has_key(userid):
-                    user_rate_list[userid].append((itemid, rating, time, location))
+                if ignore_flag == 0:
+                    if user_rate_list.has_key(userid):
+                        user_rate_list[userid].append((itemid, rating, time, location))
+                    else:
+                        user_rate_list[userid] = [(itemid, rating, time, location)]
                 else:
-                    user_rate_list[userid] = [(itemid, rating, time, location)]
+                    if user_rate_list_ground_truth.has_key(userid):
+                        user_rate_list_ground_truth[userid].append((itemid, rating, time, location))
+                    else:
+                        user_rate_list_ground_truth[userid] = [(itemid, rating, time, location)]
 
     print 'user count =',len(user_set)
     print 'item count =',len(item_set)
 
+
+    # recommendation
     target_user = '10'
-    print recommend(target_user)
-    user_ratings = user_rate_list[target_user]
+    result = recommend(target_user)
+
+    # evaluation
+    user_ratings = user_rate_list_ground_truth[target_user]
     user_ratings.sort(key=lambda x: int(x[1]), reverse=True)
-    print map(lambda x: (x[0],int(x[1])), user_ratings)
+
+    user_ratings = map(lambda x: (x[0],int(x[1])), user_ratings)
+    ground_truth = {}
+    for item in user_ratings:
+        if ground_truth.has_key(item[0]):
+            ground_truth[item[0]] += item[1]
+        else:
+            ground_truth[item[0]] = item[1]
+
+
+    print "==================================="
+    print "item, recommended_score, real_score"
+    print "------------------------------------"
+    cnt = 0
+    score = 0
+    for item in result:
+        # CG
+        if ground_truth.has_key(item[0]):
+            item_score = ground_truth[item[0]]
+        else:
+            item_score = 0
+        # DCG
+        if cnt != 0:
+            item_score /= np.log2(cnt+1)
+        print item, item_score
+        cnt = cnt+1
+        score += item_score
+        # top 10 DCG
+        if cnt == 10:
+            break
+
+    max_DCG = sorted(ground_truth.values(),reverse=True)
+    print "==================================="
+    print "recommendation result:",result
+    print "ground truth:",ground_truth
+    print "Sorted ground truth(to compute max possible DCG):",max_DCG
+    #NDCG
+    print "nDCG =",score/sum(max_DCG[0:cnt])
+
+
