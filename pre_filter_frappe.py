7ad57256d91dfe4bb70f1f93d9caa24c6ec9f2a6
baiqizhang <baiqiz@andrew.cmu.edu>
2016-04-12 19:51:47 -0700

    nDCG evaluation

--- pre_filter_frappe.py
+++ pre_filter_frappe.py
@@ -12,17 +12,21 @@ def cosine_sim(user_1, user_2, time='ANY', location='ANY'):
     cosine similarity between users using pre-filtering
     :return: a float : score
     """
-
+    if not user_rate_list.has_key(user_1) or not user_rate_list.has_key(user_2):
+        return 0
     score_1 = {}
     score_2 = {}
     N1 = 0
     N2 = 0
     N = 0
+
     for item in user_rate_list[user_1]:
-        if (time == 'ANY' or time == item[2]) and (location == 'ANY' or location == item[3]):
+        if (time == 'ANY' or time == item[2]or item[2]=='unknown') and \
+                (location == 'ANY' or location == item[3] or item[3]=='unknown'):
             score_1[item[0]] = int(item[1])
     for item in user_rate_list[user_2]:
-        if (time == 'ANY' or time == item[2]) and (location == 'ANY' or location == item[3]):
+        if (time == 'ANY' or time == item[2]or item[2]=='unknown') and \
+                (location == 'ANY' or location == item[3] or item[3]=='unknown'):
             score_2[item[0]] = int(item[1])
 
     # in case of duplication:
@@ -31,13 +35,14 @@ def cosine_sim(user_1, user_2, time='ANY', location='ANY'):
     for id in score_2:
         N2 += score_2[id] * score_2[id]
 
+    if N1==0 or N2==0:
+        return 0
+
     # calculate cosine
     for id in score_1:
         if score_2.has_key(id):
             N += score_1[id] * score_2[id]
             # print id, score_1[id], score_2[id]
-    if N1 == 0 or N2 == 0:
-        return 0
     return N / (np.sqrt(N1) * np.sqrt(N2))
 
 
@@ -54,7 +59,8 @@ def recommend(user, time='ANY', location='ANY'):
         if user == other_user:
             continue
         sum += cosine_sim(user, other_user,time,location)
-
+    if sum == 0:
+        return []
     score_final = {} # = sum{score/count}
     for other_user in user_rate_list:
         if user == other_user:
@@ -67,7 +73,8 @@ def recommend(user, time='ANY', location='ANY'):
 
         for item in user_rate_list[other_user]:
             # pre filtering
-            if (time == 'ANY' or time == item[2]) and (location == 'ANY' or location == item[3]):
+            if (time == 'ANY' or time == item[2]or item[2]=='unknown') and \
+                (location == 'ANY' or location == item[3] or item[3]=='unknown'):
                 id = item[0]
                 rate = float(item[1])
                 # if id == 'tt0266543':
@@ -99,6 +106,73 @@ def recommend(user, time='ANY', location='ANY'):
     result.sort(key=lambda x: x[1], reverse=True)
     return result
 
+
+def evaluation(user_set, time='ANY',location='ANY',algorithm = 'NO FILTER'):
+    all_result = []
+    for target_user in user_set:
+        if algorithm=='NO FILTER':
+            result = recommend(target_user)#,time=time,location = location)
+        else:
+            result = recommend(target_user,time=time,location = location)
+
+        # evaluation
+        if not user_rate_list_ground_truth.has_key(target_user):
+            continue
+        user_ratings = user_rate_list_ground_truth[target_user]
+        user_ratings.sort(key=lambda x: int(x[1]), reverse=True)
+
+        # user_ratings = map(lambda x: (x[0],int(x[1])), user_ratings)
+        ground_truth = {}
+        for item in user_ratings:
+            print item[2],item[3]
+            if time != 'ANY' and item[2]!='unknown' and item[2] != time:
+                continue
+            if location != 'ANY' and item[3]!='unknown' and item[3] != location:
+                continue
+            if ground_truth.has_key(item[0]):
+                ground_truth[item[0]] += int(item[1])
+            else:
+                ground_truth[item[0]] = int(item[1])
+
+
+        print "==================================="
+        print "item, recommended_score, real_score"
+        print "------------------------------------"
+        cnt = 0
+        score = 0
+        for item in result:
+            # CG
+            if ground_truth.has_key(item[0]):
+                item_score = ground_truth[item[0]]
+            else:
+                item_score = 0
+            # DCG
+            if cnt != 0:
+                item_score /= np.log2(cnt+1)
+            print item, item_score
+            cnt = cnt+1
+            score += item_score
+            # top 10 DCG
+            if cnt == 10:
+                break
+
+        max_DCG = sorted(ground_truth.values(),reverse=True)
+        print "==================================="
+        print "recommendation result:",result
+        print "ground truth:",ground_truth
+        print "Sorted ground truth(to compute max possible DCG):",max_DCG
+        #NDCG
+        if sum(max_DCG[0:cnt]) ==0:
+            nDCG = 0
+        else:
+            nDCG = score/sum(max_DCG[0:cnt])
+        print "nDCG =",nDCG
+        all_result.append(nDCG)
+
+
+    return all_result
+
+
 # main
 if __name__ == '__main__':
 
@@ -119,66 +193,25 @@ if __name__ == '__main__':
                 ignore_flag = 1
 
             # only use data where time and location are both available
-            if location != 'NA' and time != 'NA':
-                if ignore_flag == 0:
-                    if user_rate_list.has_key(userid):
-                        user_rate_list[userid].append((itemid, rating, time, location))
-                    else:
-                        user_rate_list[userid] = [(itemid, rating, time, location)]
+            #if location != 'NA' and time != 'NA':
+            if ignore_flag == 0:
+                if user_rate_list.has_key(userid):
+                    user_rate_list[userid].append((itemid, rating, time, location))
                 else:
-                    if user_rate_list_ground_truth.has_key(userid):
-                        user_rate_list_ground_truth[userid].append((itemid, rating, time, location))
-                    else:
-                        user_rate_list_ground_truth[userid] = [(itemid, rating, time, location)]
+                    user_rate_list[userid] = [(itemid, rating, time, location)]
+            else:
+                if user_rate_list_ground_truth.has_key(userid):
+                    user_rate_list_ground_truth[userid].append((itemid, rating, time, location))
+                else:
+                    user_rate_list_ground_truth[userid] = [(itemid, rating, time, location)]
 
     print 'user count =',len(user_set)
     print 'item count =',len(item_set)
 
 
-    # recommendation
-    target_user = '10'
-    result = recommend(target_user)
-
-    # evaluation
-    user_ratings = user_rate_list_ground_truth[target_user]
-    user_ratings.sort(key=lambda x: int(x[1]), reverse=True)
-
-    user_ratings = map(lambda x: (x[0],int(x[1])), user_ratings)
-    ground_truth = {}
-    for item in user_ratings:
-        if ground_truth.has_key(item[0]):
-            ground_truth[item[0]] += item[1]
-        else:
-            ground_truth[item[0]] = item[1]
-
-
-    print "==================================="
-    print "item, recommended_score, real_score"
-    print "------------------------------------"
-    cnt = 0
-    score = 0
-    for item in result:
-        # CG
-        if ground_truth.has_key(item[0]):
-            item_score = ground_truth[item[0]]
-        else:
-            item_score = 0
-        # DCG
-        if cnt != 0:
-            item_score /= np.log2(cnt+1)
-        print item, item_score
-        cnt = cnt+1
-        score += item_score
-        # top 10 DCG
-        if cnt == 10:
-            break
-
-    max_DCG = sorted(ground_truth.values(),reverse=True)
-    print "==================================="
-    print "recommendation result:",result
-    print "ground truth:",ground_truth
-    print "Sorted ground truth(to compute max possible DCG):",max_DCG
-    #NDCG
-    print "nDCG =",score/sum(max_DCG[0:cnt])
-
-
+    nofilter = evaluation(user_set,'morning','ANY')
+    prefilter = evaluation(user_set,'morning','ANY','PRE')
+    print nofilter
+    print np.mean(nofilter),np.median(nofilter),np.histogram(nofilter)
+    print prefilter
+    print np.mean(prefilter),np.median(prefilter),np.histogram(prefilter)
